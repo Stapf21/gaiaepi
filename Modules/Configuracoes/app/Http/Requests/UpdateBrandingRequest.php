2f87cf0f7bfee93e0b36cff8e4a2477ec40ef322
Stapf21 <matheusstapf@gmail.com>
2026-02-13 21:15:46 -0300

    fix: remove leading whitespace before php tag

--- Modules/Configuracoes/app/Http/Requests/UpdateBrandingRequest.php
+++ Modules/Configuracoes/app/Http/Requests/UpdateBrandingRequest.php
@@ -1,29 +1,29 @@
-    <?php
+<?php
 
-    namespace Modules\Configuracoes\Http\Requests;
+namespace Modules\Configuracoes\Http\Requests;
 
-    use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Foundation\Http\FormRequest;
 
-    class UpdateBrandingRequest extends FormRequest
+class UpdateBrandingRequest extends FormRequest
+{
+    public function authorize(): bool
     {
-        public function authorize(): bool
-        {
-            return $this->user()?->can('manage_branding') ?? false;
-        }
-
-        public function rules(): array
-        {
-            return [
-                'logo' => ['nullable', 'image', 'max:2048'],
-                'favicon' => ['nullable', 'file', 'mimes:png,ico,svg', 'max:1024'],
-                'login_background' => ['nullable', 'image', 'max:5120'],
-                'login_overlay_gif' => ['nullable', 'file', 'mimes:gif,webp', 'max:10240'],
-                'logo_url' => ['nullable', 'url', 'max:2048'],
-                'favicon_url' => ['nullable', 'url', 'max:2048'],
-                'login_background_link' => ['nullable', 'url', 'max:2048'],
-                'login_overlay_gif_url' => ['nullable', 'url', 'max:2048', 'regex:/^https?:\/\/.+\.(gif|webp)(\?.*)?$/i'],
-                'clear_login_background' => ['sometimes', 'boolean'],
-                'clear_login_overlay_gif' => ['sometimes', 'boolean'],
-            ];
-        }
+        return $this->user()?->can('manage_branding') ?? false;
     }
+
+    public function rules(): array
+    {
+        return [
+            'logo' => ['nullable', 'image', 'max:2048'],
+            'favicon' => ['nullable', 'file', 'mimes:png,ico,svg', 'max:1024'],
+            'login_background' => ['nullable', 'image', 'max:5120'],
+            'login_overlay_gif' => ['nullable', 'file', 'mimes:gif,webp', 'max:10240'],
+            'logo_url' => ['nullable', 'url', 'max:2048'],
+            'favicon_url' => ['nullable', 'url', 'max:2048'],
+            'login_background_link' => ['nullable', 'url', 'max:2048'],
+            'login_overlay_gif_url' => ['nullable', 'url', 'max:2048', 'regex:/^https?:\\/\\/.+\\.(gif|webp)(\\?.*)?$/i'],
+            'clear_login_background' => ['sometimes', 'boolean'],
+            'clear_login_overlay_gif' => ['sometimes', 'boolean'],
+        ];
+    }
+}
